f641d1b571c60d929a6e4510235cc654794d7ef1
jay <dj.min43@gmail.com>
2022-12-24 11:07:58 +0900

    chore

--- bug-reproduction/src/routes/index.tsx
+++ bug-reproduction/src/routes/index.tsx
@@ -1,16 +1,33 @@
+import {createResource, createSignal, JSX} from "solid-js";
+
+const fetchChar = (value: string) => {
+    return api.findChar(value)
+}
+
 export default function Home() {
+
+    const [searchInput, setSearchInput] = createSignal("")
+
+    const [query] = createResource(searchInput, () => fetchChar(searchInput()))
+
+    const onInput: JSX.EventHandler<HTMLInputElement, InputEvent> = (event ) => {
+        setSearchInput(event.currentTarget.value);
+    };
+
   return (
     <main>
         <h1>bug reproduction</h1>
-
+        <label>file:</label>
+        <input type="text" value={searchInput()} onInput={onInput} />
+        {query()}
     </main>
   );
 }
 
-const numberList = [0, 1, 2, 3, 4, 5, 6, 7]
+const charList = ["a", "b", "c", "d", "e"]
 
 const api = {
-    findNumber: async (value: number) => {
-        return numberList.includes(value)
+    findChar: async (value: string) => {
+        return charList.find(element => element === value)
     }
 }
